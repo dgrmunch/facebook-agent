ee1d7630c4b7d9b7e0bb84381915538a18284d36
Diego González Rodríguez <xmunch@xmunch.com>
2014-12-06 11:26:44 +0100

    Add updateFriends() method to FacebookAgent

--- src/main/java/com/xmunch/facebook/model/FacebookAgent.java
+++ src/main/java/com/xmunch/facebook/model/FacebookAgent.java
@@ -3,7 +3,13 @@
  */
 package com.xmunch.facebook.model;
 
+import java.util.Iterator;
+import java.util.List;
+
 import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.FacebookProfile;
+import org.springframework.social.facebook.api.PagedList;
+import org.springframework.social.facebook.api.Reference;
 
 /**
  * @author xmunch
@@ -12,6 +18,7 @@ import org.springframework.social.facebook.api.Facebook;
 public class FacebookAgent extends Thread {
 
 	private Facebook facebook;
+	private List<FacebookFriend> friends;
 	
 	public FacebookAgent(Facebook facebook) {
 		this.setFacebook(facebook);
@@ -19,6 +26,7 @@ public class FacebookAgent extends Thread {
 	
 	public void run() {
 		System.out.println("Facebook agent is running");
+		updateFriends();
 	}
 
 	public Facebook getFacebook() {
@@ -27,6 +35,27 @@ public class FacebookAgent extends Thread {
 
 	public void setFacebook(Facebook facebook) {
 		this.facebook = facebook;
+	}
+
+	private void updateFriends() {
+		Reference friendPointer;
+		FacebookProfile friendProfile;
+		FacebookFriend friend;
+		PagedList<Reference> friendsPagedList = this.facebook
+				.friendOperations().getFriends();
+		Iterator<Reference> iterator = friendsPagedList.iterator();
+
+		while (iterator.hasNext()) {
+			friendPointer = iterator.next();
+			friendProfile = facebook.userOperations().getUserProfile(
+					friendPointer.getId());
+			friend = new FacebookFriend(friendProfile.getId(), friendProfile);
+			if (!friends.contains(friend)) {
+				friends.add(friend);
+			}
+		}
+
+		System.out.println("The list of friends has been updated with " + friends.size() + " friends.");
 	}
 
 }
