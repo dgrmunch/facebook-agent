82c1dabb643a16b526226b1c85aa44c01ff176de
Diego González Rodríguez <xmunch@xmunch.com>
2014-12-07 21:16:12 +0100

    Use GlobalValues in FacebookController

--- src/main/java/com/xmunch/facebook/FacebookController.java
+++ src/main/java/com/xmunch/facebook/FacebookController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.google.gson.Gson;
+import com.xmunch.facebook.aux.GlobalValues;
 import com.xmunch.facebook.model.FacebookAgent;
 
 @Controller
@@ -27,11 +28,11 @@ public class FacebookController {
     @RequestMapping(method=RequestMethod.GET)
     public String helloFacebook(Model model) {
         if (!facebook.isAuthorized()) {
-            return "redirect:/connect/facebook";
+            return GlobalValues.FACEBOOK_REDIRECT.get();
         } else {
         	 this.fa = new FacebookAgent(facebook);	
-        	 model.addAttribute("json",gson.toJson(fa.getFriends()));
-             return "done";
+        	 model.addAttribute(GlobalValues.JSON_ATTRIBUTE.get(),gson.toJson(fa.getFriends()));
+             return GlobalValues.DONE_TEMPLATE.get();
         }
     }
 }
